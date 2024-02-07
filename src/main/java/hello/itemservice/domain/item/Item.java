b981f8fdb7a0b21cee55d08253dd928840dfb86c
Zayne <star1582@naver.com>
2024-02-07 17:04:05 +0900

    adding checkbox

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,6 +2,9 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 
+import java.util.List;
+
+
 @Data
 public class Item {
 
@@ -9,6 +12,11 @@ public class Item {
     private String itemName;
     private Integer price;
     private Integer quantity;
+
+    private Boolean open;
+    private List<String> regions;
+    private ItemType itemType;
+    private String deliveryCode;
 
     public Item() {
     }
